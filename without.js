f66174efb230ecbb3a51ae41e6ae9b12e526b3ec
Mathew Charm <mattcharmz@gmail.com>
2023-04-06 14:57:00 -0700

    removing items

---
+++ without.js
@@ -0,0 +1,33 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = (source, itemsToRemove) => {
+  let result = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      result.push(source[i])
+    }
+  }
+  return result;
+}
+
+//test
+assertEqual(eqArrays(without([1, 2, 3], [1])));
+assertEqual(eqArrays(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]), true); // => true
