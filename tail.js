66de87192cf484de2366a536cefefd573c3242c4
Mathew Charm <mattcharmz@gmail.com>
2023-04-10 19:49:45 -0700

    refactored

--- tail.js
+++ tail.js
@@ -5,7 +5,7 @@ const tail = (array) => {
   return array.slice(1);
 };
 
-
+module.exports = tail;
 // Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
